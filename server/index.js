cacd53ead7af756ee62dfd0addb1a4e5f65a5341
fmpro12 <yank@checkpoint.com>
2019-04-29 16:30:57 +0300

    heroku

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ const users = require('../server/users/server')
 app.use(cors())
 app.use(bodyParser.json());
 app.use("/api/users/", users);
+app.get('/', function(req, res) {
+
+  // ejs render automatically looks in the views folder
+  res.render(__dirname, '../public/index');
+});
 
 app.use(express.static(distFolder))
 
